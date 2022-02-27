819178e92d2de2738c816032f8e36627e0ebea94
ankitsinghrana13 <ak9897432538@gmail.com>
2022-02-28 06:19:04 +0800

    Add project files.

---
+++ Constructor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Constructor
+{
+    class emply
+    {
+        public int a;
+        public int b;
+        public int c;
+      
+        public emply(int a,int b,int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+          
+        }
+
+        public void Getdata()
+        {
+            c = a + b;
+            Console.WriteLine("total=" + this.c);
+            Console.WriteLine("total=" + c);
+        }
+    }
+    class program
+    {
+       static void Main(string[] arg)
+        {
+            emply obj1 = new emply(10, 20, 30);
+            obj1.Getdata();
+
+        }
+    }
+}
